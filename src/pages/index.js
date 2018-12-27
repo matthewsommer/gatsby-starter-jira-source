1d37b1709069cf207fc14af7220e9329f42469bf
Matt <matthew.sommer@gmail.com>
2018-12-27 08:56:46 -0500

    updated homepage copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,8 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Welcome to a Gatsby website powered by Jira!</h1>
+    <p>This is a static website that is built from data pulled from Jira.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
